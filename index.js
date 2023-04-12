0d1bf18d848569ce986c85bd12194e2864263f58
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-12 17:22:53 +0100

    Added highlight effect to hovered squares

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ const gridSizeInput = document.getElementById('gridSizeInput');
 
 let gridSize = gridSizeInput.value;
 
-gridSizeInput.addEventListener('change', function(){
+gridSizeInput.addEventListener('change', function () {
     gridSize = gridSizeInput.value;
     createGrid(gridSize);
 })
 
-function createGrid(size){
+function createGrid(size) {
     grid.innerHTML = '';
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    for (let i = 0; i < size*size; i++){
+    for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+
+        square.addEventListener('mouseenter', function (e) {
+            if(e.target.classList.contains("square")){
+                e.target.classList.toggle('highlighted');
+            }
+        });
+
+        square.addEventListener('mouseleave', function (e) {
+            if(e.target.classList.contains("square")){
+                e.target.classList.toggle('highlighted');
+            }
+        });
+
         grid.appendChild(square);
     }
 }
+
+
+
 
 createGrid(gridSize);
