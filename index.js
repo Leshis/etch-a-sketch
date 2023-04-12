92024d77e4a4d9c307d291902bc6a742c2407b79
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-12 17:03:49 +0100

    Implemented slider value input

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const gridSize = 16;
-
 const grid = document.getElementById('grid');
+const gridSizeInput = document.getElementById('gridSizeInput');
 
+let gridSize = gridSizeInput.value;
 
+gridSizeInput.addEventListener('change', function(){
+    gridSize = gridSizeInput.value;
+    createGrid(gridSize);
+})
 
 function createGrid(size){
-    for (let i = 0; i < gridSize*gridSize; i++){
+    grid.innerHTML = '';
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size*size; i++){
         const square = document.createElement('div');
         square.classList.add('square');
         grid.appendChild(square);
