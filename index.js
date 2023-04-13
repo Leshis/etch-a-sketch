f1d95104602b0d9a967cdbf889950d71fdffa199
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-13 10:10:31 +0100

    Added colour picker and basic styles

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const grid = document.getElementById('grid');
-const gridSizeInput = document.getElementById('gridSizeInput');
+const gridSizeInput = document.getElementById('grid-size-input');
+const colourInput = document.getElementById('color-picker');
+const colourBtn = document.getElementById('color-button');
 
+const defaultColour = 'darkslategray';
+
+let highlightClass = document.getElementsByClassName('.highlighted');
 let gridSize = gridSizeInput.value;
+let colour = colourInput.value;
+let isColour = false;
 
-gridSizeInput.addEventListener('change', function () {
+gridSizeInput.addEventListener('change', function(){
     gridSize = gridSizeInput.value;
     createGrid(gridSize);
+})
+
+colourBtn.addEventListener('click', function(){
+    colourBtn.classList.toggle('btn-highlight');
+    isColour = !isColour;
+    if(!isColour){
+        colour = defaultColour;
+    }
 })
 
 function createGrid(size) {
@@ -15,16 +30,22 @@ function createGrid(size) {
         let square = document.createElement('div');
         square.classList.add('square');
 
+        
         square.addEventListener('mouseenter', function (e) {
-            e.target.classList.toggle('highlighted');
+            if (isColour) {
+                e.target.style.backgroundColor = colourInput.value ? colourInput.value: 'darkslategray';
+            } else {
+                e.target.style.backgroundColor = defaultColour;
+            }
         });
 
-        square.addEventListener('mouseleave', function (e) {
-            e.target.classList.toggle('highlighted');
-        });
+        square.addEventListener('mouseleave', function(e){
+            e.target.style.backgroundColor = 'gainsboro';
+        })
 
         grid.appendChild(square);
     }
 }
 
 createGrid(gridSize);
+console.log(colour);
